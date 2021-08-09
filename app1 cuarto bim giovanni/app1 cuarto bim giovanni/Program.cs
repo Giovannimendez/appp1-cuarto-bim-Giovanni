95dadfd416cea137b138f5effa6c508c00f92b9f
Giovannimendez <85662723+Giovannimendez@users.noreply.github.com>
2021-08-09 18:56:45 -0500

    Add files via upload

---
+++ app1 cuarto bim giovanni/app1 cuarto bim giovanni/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace app1_cuarto_bim_giovanni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string R = "s";
+            while (R == "s")
+            {
+                Console.WriteLine("Escribe el nombre del arhivo");
+                string archivo = Console.ReadLine();
+
+                StreamWriter ar;
+                ar = File.CreateText(archivo + ".txt");
+                ar.WriteLine("Archivo Creado");
+                ar.Close();
+
+                Console.WriteLine("Quieres crear otro archivo?");
+                Console.WriteLine("s/n");
+                R = Console.ReadLine();
+                Console.Clear();
+                if (R == "no")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
